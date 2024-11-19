6fda995a48638bc6fcbb7d0d78c277d1e4e1789f
ventraman <venkat.r@kalvium.community>
2024-11-19 11:25:00 +0530

    set-up-database

--- Frontend/blog/src/Firebase/Firebase.jsx
+++ Frontend/blog/src/Firebase/Firebase.jsx
@@ -2,11 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Correct import for Firestore
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDg-mm5zs0IuW59WiRP-dbQFGtiifVLfUw",
   authDomain: "blog-4dc9a.firebaseapp.com",
@@ -21,3 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app); 
+export const db = getFirestore(app); // Correct initialization for Firestore
